1f4ac5c1cfcac658d6cf9dabc3b62244b6e4d248
Martynas <martynas.viskontas@gmail.com>
2017-10-14 21:55:51 +0300

    implement load more products functionality

--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Product } from '../product';
 
-export const ProductList = ({filteredProducts, productsMap}) => {
+export const ProductList = ({filteredProducts, productsMap, loadMoreData, limit}) => {
+  const showLoadMoreData = limit < Object.keys(productsMap).length;
   return (
     <div>
-      {filteredProducts.map((id, index) => 
-        (<Product key={index} product={productsMap[id]} />)
-      )}
+      {filteredProducts.map((id, index) =>  (<Product key={index} product={productsMap[id]} />))}
+      {
+        showLoadMoreData ?
+          <div onClick={loadMoreData}>Load more data</div> :
+          null 
+      }
     </div>
   )
 }
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -4,25 +4,23 @@ import { fetchData } from '../../apiMock';
 import { receiveData } from '../../redux/actions';
 import { ProductGroups, ProductFilter, ProductList } from '../../components';
 
+const PRODUCT_LIMIT = 6;
+
 class HomePageComponent extends Component {
 
-  state: {
-    filteredProducts: [],
-    productLimit: 6,
-    priceMin: 0,
-    priceMax: 0,
-    priceRange: []
-  }
+  state: {}
 
   componentWillReceiveProps(nextProps) {
     const products = nextProps.products;
     const {priceMin, priceMax} = this.getPriceRange(products);
-    const filteredProducts = Object.keys(products);
+    const filteredProducts = Object.keys(products).slice(0, PRODUCT_LIMIT);
 
     this.setState({
       priceMin,
       priceMax,
       filteredProducts,
+      products,
+      productLimit: PRODUCT_LIMIT,
       priceRange: [priceMin, priceMax]
     })
   }
@@ -41,36 +39,51 @@ class HomePageComponent extends Component {
     };
   }
 
-  getFilteredProducts({priceMin, priceMax, productLimit}) {
-    const products = this.props.products;
+  getFilteredProducts({priceMin, priceMax, productLimit, products}) {
     return Object.keys(products)
-      .filter(id => products[id].price >= priceMin && products[id].price <= priceMax)
+      .filter(id => {
+        const price = products[id].price;
+        return price >= priceMin && price <= priceMax
+      })
       .slice(0, productLimit)
   }
 
   onPriceRangeChange = ([priceMin, priceMax]) => {
-    const filteredProducts = this.getFilteredProducts({priceMin, priceMax, productLimit: this.state.productLimit});
+    const {productLimit, products} = this.state;
+    const filteredProducts = this.getFilteredProducts({priceMin, priceMax, productLimit, products});
     this.setState({
       priceMin,
       priceMax,
       filteredProducts
     })
-  }  
+  }
+
+  onLoadMoreData = () => {
+    const {products, productLimit: limit, priceMin, priceMax} = this.state;
+    const productCount = Object.keys(products).length; 
+    const productLimit = productCount - limit < PRODUCT_LIMIT ? productCount : limit + PRODUCT_LIMIT;
+    const filteredProducts = this.getFilteredProducts({products, priceMin, priceMax, productLimit})
+
+    this.setState({productLimit, filteredProducts})
+  }
 
   render() {
     if (this.state) {
+      const {priceMin, priceMax, priceRange, filteredProducts, products, productLimit} = this.state;
       return (
         <div>
           <ProductGroups />
           <ProductFilter 
-            priceMin={this.state.priceMin}
-            priceMax={this.state.priceMax}
-            priceRange={this.state.priceRange}
+            priceMin={priceMin}
+            priceMax={priceMax}
+            priceRange={priceRange}
             onChange={this.onPriceRangeChange}
           />
           <ProductList 
-            filteredProducts={this.state.filteredProducts}
-            productsMap={this.props.products}
+            filteredProducts={filteredProducts}
+            productsMap={products}
+            loadMoreData={this.onLoadMoreData}
+            limit={productLimit}
           />
         </div>
       )
